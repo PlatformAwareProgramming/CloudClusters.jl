2e2b2918b3f30a9a65ac530d4a8711c301a20861
joaomarceloalencar <joao.marcelo@ufc.br>
2024-01-09 10:18:41 -0300

    Exemplo de como recuperar informações dos tipos de instâncias da AWS.

--- src/CloudClusters.jl
+++ src/CloudClusters.jl
@@ -1,5 +1,33 @@
 module CloudClusters
+#=
+For this module to work, you`ll need AWS credentials in the ${HOME}/.aws directory. 
+=# 
 
-greet() = print("Hello World!")
+using Printf
+using AWS: @service
+@service Ec2
 
-end # module CloudClusters
+export retrieve_instance_types
+export print_info
+
+function retrieve_instance_types()
+    response = Ec2.describe_instance_types()
+    instance_types = response["instanceTypeSet"]["item"]
+
+    while haskey(response, "nextToken")
+        nextToken = response["nextToken"]
+        params = Dict("NextToken" => nextToken)
+        response = Ec2.describe_instance_types(params)
+        instance_types = vcat(instance_types, response["instanceTypeSet"]["item"])
+    end
+    instance_types
+end
+
+function print_info()
+    instance_types = retrieve_instance_types()
+    for i in 1:length(instance_types)
+        @printf("%20s: %5s cores %12s MiB of RAM\n", instance_types[i]["instanceType"], instance_types[i]["vCpuInfo"]["defaultVCpus"], instance_types[i]["memoryInfo"]["sizeInMiB"] )
+    end
+end
+
+end # end CloudCluster
