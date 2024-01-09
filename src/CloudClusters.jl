927fbf0d24c0bb9bd99e2830246e59719717d4aa
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2024-01-09 15:09:39 -0300

    structuring project

--- src/CloudClusters.jl
+++ src/CloudClusters.jl
@@ -1,33 +1,11 @@
 module CloudClusters
-#=
-For this module to work, you`ll need AWS credentials in the ${HOME}/.aws directory. 
-=# 
 
-using Printf
-using AWS: @service
-@service Ec2
+input("testesjm.jl")
 
-export retrieve_instance_types
-export print_info
+input("cluster.jl")
+input("deploy.jl")
+input("resolve.jl")
+input("macros.jl")
 
-function retrieve_instance_types()
-    response = Ec2.describe_instance_types()
-    instance_types = response["instanceTypeSet"]["item"]
-
-    while haskey(response, "nextToken")
-        nextToken = response["nextToken"]
-        params = Dict("NextToken" => nextToken)
-        response = Ec2.describe_instance_types(params)
-        instance_types = vcat(instance_types, response["instanceTypeSet"]["item"])
-    end
-    instance_types
-end
-
-function print_info()
-    instance_types = retrieve_instance_types()
-    for i in 1:length(instance_types)
-        @printf("%20s: %5s cores %12s MiB of RAM\n", instance_types[i]["instanceType"], instance_types[i]["vCpuInfo"]["defaultVCpus"], instance_types[i]["memoryInfo"]["sizeInMiB"] )
-    end
-end
 
 end # end CloudCluster
