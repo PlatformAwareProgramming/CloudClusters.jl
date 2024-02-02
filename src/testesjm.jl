19c9467451a5bacd8ff5cd87c9952d09cc2a483f
joaomarceloalencar <joao.marcelo@ufc.br>
2024-02-01 21:27:07 -0300

    Usar o modelo do CloudFormation é inviável. Tentando agora invocar o comando pcluster.

--- src/testesjm.jl
+++ src/testesjm.jl
@@ -14,20 +14,29 @@ using AWS: @service
 export retrieve_instance_types
 export print_info
 
-#=
+"""
+    Função para invocar pcluster
+"""
+function pcluster(commands)
+    virtualenv = "source ~/apc-ve/bin/activate; pcluster $commands;"
+    process = `bash -c $virtualenv`
+    run(process)
+end
+
+"""
     This function creates a random bucket in the default region.
     It returns the name of the bucket.
-=#
+"""
 function create_random_bucket()
     bucket_name = "bucket$(lowercase(randstring(10)))"
     S3.create_bucket(bucket_name)
     bucket_name
 end
 
-#=
+"""
     This function puts a template file in a bucket.
     It returns the url of the object.
-=#
+"""
 function put_template_bucket(template_file, bucket_name)
     open(template_file, "r") do file
         S3.put_object(bucket_name, "template.yaml", Dict("body" => read(file, String)))
@@ -36,9 +45,55 @@ function put_template_bucket(template_file, bucket_name)
     url
 end
 
+"""
+    This function removes bucket. To do so, it must first
+    remote all the files. Then, it can remove the bucket.
+"""
 function delete_random_bucket(bucket_name)
     S3.delete_object(bucket_name, "template.yaml")
     S3.delete_bucket(bucket_name)
+end
+
+"""
+    create_cluster(region, instance_type, subnet_id, key_name, min_count, max_count)
+        1. Loads the example template.
+        2. Customize it and save it to a random file.
+        3. Creates the bucket.
+        4. Upload the random template file to the bucket.
+        5. Creates the CloudFormation stack based on the template.
+        6. Removes the random bucket.
+        7. Returns the stack Id and the IP?
+
+"""
+function create_cluster(instance_type, subnet_id, key_name, min_count, max_count)
+    template = YAML.load_file("artifacts/cluster-template.yaml")
+    template["Resources"]["HeadNodeLaunchTemplate"]["Properties"]["LaunchTemplateData"]["InstanceType"] = instance_type
+    template["Resources"]["HeadNodeLaunchTemplate"]["Properties"]["LaunchTemplateData"]["KeyName"] = key_name
+
+    template["Resources"]["HeadNode"]["Properties"]["Networking"]["SubnetId"] = subnet_id
+    
+    template["Resources"]["ComputeNode"]["Properties"]["MinCount"] = min_count
+    template["Resources"]["ComputeNode"]["Properties"]["MaxCount"] = max_count
+    
+    open("random.yaml", "w") do file
+        write(file, YAML.dump(template))
+    end
+
+    bucket_name = create_random_bucket()
+    url = put_template_bucket("random.yaml", bucket_name)
+
+    #=
+    stack_id = CloudFormation.create_stack(region, url)
+    delete_random_bucket(bucket_name)
+
+    stack_id
+    =#
+end
+
+"""
+    This function should destroy the cluster using the CloudFormation service.
+"""
+function delete_cluster(stack_id)
 end
 
 function retrieve_instance_types()
