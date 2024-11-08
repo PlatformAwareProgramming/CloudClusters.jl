70e3470c2e9ee7127869ff16baf0d72839939b6c
Francisco Heron de Carvalho Junior <102302676+decarvalhojunior-fh@users.noreply.github.com>
2024-11-08 15:35:27 -0300

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,5 +5,5 @@ makedocs(sitename="CloudClusters.jl")
 repo = "github.com/PlatformAwareProgramming/CloudClusters.jl.git"
 
 withenv("GITHUB_REPOSITORY" => repo) do
-  deploydocs(; repo, versions=["stable" => "v^", "dev" => "dev"])
+  deploydocs(; repo, versions=["stable" => "v^", "dev" => "dev"], push_preview = true, forcepush = true)
 end
